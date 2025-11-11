03cab62eb0ac1afa798c7724c0de307f89eff16f
Liviaroy <2467844@cegepmontpetit.ca>
2025-11-11 15:39:21 -0500

    ..

---
+++ cours 9/question8.py
@@ -0,0 +1,14 @@
+virus = ["Virus-T", "Virus-C", "Virus-G", "Uroboros"]
+
+virus_et_autres_info = [
+    ["Virus-T", 0.9 ,"Transforme les humains en zombies et armes biologiques"],
+    ["Virus-C", 0.0001 ,"Provoque des mutations extrêmes et régénère les tissus"],
+    ["Virus-G", 0.85, "Cause des mutations incontrôlables avec régénération cellulaire rapide"],
+    ["Uroboros", 0.005, "Consomme les organismes incompatibles et renforce les hôtes compatibles"]
+]
+
+if len(virus) == 0:
+    print("Désolé aucun produit pharmaceutique mortel disponible")
+
+
+
